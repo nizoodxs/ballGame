f6f30f55cb34401b142cd5409e7c7cd7aae79e1b
nizoodxs <subedi275@gmail.com>
2016-11-27 22:31:14 +0545

    First commit

---
+++ Assets/scripts/coinController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class coinController : MonoBehaviour {
+
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3(15, 45, 30) * Time.deltaTime);
+	}
+}
---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private int count;
+
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+
+	//at first frame of script loading
+	void Start(){
+		rb = GetComponent<Rigidbody> ();
+		count = 0;
+		setCountText ();
+		winText.text = "";
+	}
+
+
+    //right before doing any physics calculation
+    void FixedUpdate()
+    {
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal,0.0f,moveVertical);
+
+		rb.AddForce (movement * speed);
+		
+    }
+		
+	//testing the trigger to another object
+	void OnTriggerEnter(Collider other) {
+		
+		if (other.gameObject.CompareTag ("coin")) {
+			other.gameObject.SetActive (false);
+			count++;
+			setCountText ();
+		}
+
+	}
+
+	void setCountText(){
+		countText.text = "Score: " + count.ToString ();
+		if (count >= 12) {
+			winText.text = "You Win !!!!";
+		}
+	}
+
+}
